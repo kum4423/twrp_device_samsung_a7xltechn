0bc4c4dc3ad0e3a441d022f64c21b4e64162ee6b
kum4423 <kumpei.suzuki@gmail.com>
2020-06-08 22:36:38 +0900

    Delete permissive.sh

--- permissive.sh
+++
@@ -1,4 +0,0 @@
-# Get your device's block path where "system", "recovery", etc. lives.
-# That can be "/dev/block/bootdevice/by-name" or something like that.
-mkdir -p /dev/block/platform/7824900.sdhci/by-name/
-busybox mount -o bind /dev/block/platform/7824900.sdhci/by-name/ /dev/block/platform/7824900.sdhci/by-name/
